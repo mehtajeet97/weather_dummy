94792257250122a0827ef92cb3ea7a1b978f75ec
Jeet Mehta <mehta.jeet97@gmail.com>
2024-02-29 11:58:27 -0500

    Comments added for better understanding

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// fetchWeather is the eventlistener (onclick)
 async function fetchWeather() {
   let searchInput = document.getElementById("search").value;
   const weatherDataSection = document.getElementById("weather-data");
@@ -5,6 +6,7 @@ async function fetchWeather() {
   /* Uncomment this line once you have entered the value for your own apikey
   const apiKey = ; */
 
+  // In case no input is searched
   if (searchInput == "") {
     weatherDataSection.innerHTML = `
     <div>
@@ -14,7 +16,7 @@ async function fetchWeather() {
     `;
     return;
   }
-
+  // Function to get longitude and latitude based on the name (city) provided
   async function getLonAndLat() {
     const countryCode = 1;
     geocodeURL = `http://api.openweathermap.org/geo/1.0/direct?q=${searchInput.replace(
@@ -22,11 +24,14 @@ async function fetchWeather() {
       "%20"
     )},${countryCode}&limit=1&appid=${apiKey}`;
     const response = await fetch(geocodeURL);
+
+    // If the response is not 200 or good
     if (!response.ok) {
       console.log("Bad response! ", response.status);
       return;
     }
     const data = await response.json();
+    // If empty data is returned
     if (data.length == 0) {
       console.log("Something went wrong here.");
       weatherDataSection.innerHTML = `
@@ -40,9 +45,13 @@ async function fetchWeather() {
       return data[0];
     }
   }
+
+  // Function to provide weather information using API wherein longitude and latitude is provided as input
   async function getWeatherData(lon, lat) {
     const weatherURL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`;
     const response = await fetch(weatherURL);
+
+    // If the response is not 200 or good
     if (!response.ok) {
       console.log("Bad response! ", response.status);
       return;
@@ -62,8 +71,9 @@ async function fetchWeather() {
     </div>
     `;
   }
-
+  // To revert the textbox to empty
   document.getElementById("search").value = " ";
+  // To provide output
   const geocodeData = await getLonAndLat();
   getWeatherData(geocodeData.lon, geocodeData.lat);
 }
